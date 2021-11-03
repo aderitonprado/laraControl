2144255bf1e8ed69f933fa68ea1129313458dbbc
Adériton Prado <aderitonpsantana@gmail.com>
2021-11-03 16:24:07 -0300

    Remoção do css da tela de login do app.css
    Remoção dos estilos da tela de login, do arquivo public\css\app.css para incluir na propria pagina de login. Estava conflitando com a dashboard.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,12 +30,58 @@
 
 
     <style>
+        html,
+        body {
+            height: 100%;
+        }
+
+        body {
+            display: flex;
+            align-items: center;
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: #f5f5f5;
+        }
+
+        .form-signin {
+            width: 100%;
+            max-width: 330px;
+            padding: 15px;
+            margin: auto;
+        }
+
+        .form-signin .checkbox {
+            font-weight: 400;
+        }
+
+        .form-signin .form-floating:focus-within {
+            z-index: 2;
+        }
+
+        .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+
+        .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
+
         .bd-placeholder-img {
             font-size: 1.125rem;
             text-anchor: middle;
             -webkit-user-select: none;
             -moz-user-select: none;
             user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
         }
 
         @media (min-width: 768px) {
@@ -65,11 +111,13 @@
             <h1 class="h3 mb-3 fw-normal">Entrar</h1>
 
             <div class="form-floating">
-                <input type="email" name="email" class="form-control" id="floatingInput" :value="old('email')" required autofocus placeholder="Seu e-mail">
+                <input type="email" name="email" class="form-control" id="floatingInput" :value="old('email')" required
+                    autofocus placeholder="Seu e-mail">
                 <label for="floatingInput" value="{{ __('Email') }}">Seu e-mail</label>
             </div>
             <div class="form-floating">
-                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Sua senha">
+                <input type="password" name="password" class="form-control" id="floatingPassword"
+                    placeholder="Sua senha">
                 <label for="floatingPassword">Sua senha</label>
             </div>
 
@@ -88,7 +136,7 @@
                 </label>
             </div>
             <button class="w-100 btn btn-lg btn-primary" type="submit">{{ __('Entrar') }}</button>
-            
+
             <p class="mt-5 text-muted">&copy; 2021</p>
             <p class="mb-3 text-muted">Desenvolvido por: Adériton Prado</p>
         </form>
