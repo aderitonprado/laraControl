94dcdcb89c3c62d4bf10cce703c0bf379e8565ec
Adériton Prado <aderitonpsantana@gmail.com>
2021-11-04 15:29:11 -0300

    Remoção do dompdf e referencias
    Remoção por completo da lib e suas referencias.

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -231,7 +230,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
