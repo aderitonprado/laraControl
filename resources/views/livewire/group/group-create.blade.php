38a3a677a8de88269ebff84d61bf4373fadeac48
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-23 13:56:38 -0300

    Update group-create.blade.php
    Criação efetiva da view de criação dos grupos.

--- resources/views/livewire/group/group-create.blade.php
+++ resources/views/livewire/group/group-create.blade.php
@@ -1,3 +1,40 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+<div class="mb-4">
+    <x-slot name="header">
+        <h1>Criar Grupo</h1>
+    </x-slot>
+
+    @include('includes.message')
+
+    <div class="mt-4">
+
+        <div class="container">
+            <form name="createGroup" wire:submit.prevent="createGroup">
+                <div class="row mb-5">
+                    <div class="col-sm-8">
+                        <div class="input-group">
+                            <input type="text" name="description"
+                                class="form-control @error('description') border border-danger @enderror"
+                                placeholder="Descrição do grupo" aria-label="description"
+                                aria-describedby="description" wire:model="description">
+                        </div>
+
+                        @error('description')
+                            <p class="text-danger"><small>{{ $message }}</small></p>
+                        @enderror
+                    </div>
+                </div>
+
+                <button 
+                    type="submit" 
+                    class="btn btn-lg btn-pill btn-success"
+                    >
+                    Criar Grupo
+                </button>
+            </form>
+
+        </div>
+
+    </div>
+
+
 </div>
