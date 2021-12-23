254261be85d2616d6e7e82806382261bb0a9c251
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-23 13:55:58 -0300

    Update message.blade.php
    Troca da classe bg-info para bg-success no inclide de mensagens das views.

--- resources/views/includes/message.blade.php
+++ resources/views/includes/message.blade.php
@@ -1,7 +1,7 @@
 @if (session()->has('message'))
 
     <div class="container">
-        <div class="bg-info text-white p-2">
+        <div class="bg-success text-white p-2">
             <h5>{{ session('message') }}</h5>
         </div>
     </div>
