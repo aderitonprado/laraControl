b09cde96e3125ee98f0d1d4a338f6820eae529de
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-23 11:14:56 -0300

    Update Group.php
    Relacionamento do grupo com o usuario. Assim saberemos qual usuario cadastrou o grupo.

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Group extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['description'];
+
+    public function equipaments()
+    {
+        return $this->hasMany(Equipament::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
