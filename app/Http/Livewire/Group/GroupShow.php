805a339279eda9f97ee9eed4654fd1638c084613
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-23 15:18:26 -0300

    Update GroupShow.php
    Criação da lógica da tela de visualizar componente.

--- app/Http/Livewire/Group/GroupShow.php
+++ app/Http/Livewire/Group/GroupShow.php
@@ -3,9 +3,21 @@
 namespace App\Http\Livewire\Group;
 
 use Livewire\Component;
+use App\Models\Group;
 
 class GroupShow extends Component
 {
+    public $description;
+    public $user_id;
+
+    public Group $group;
+
+    public function mount()
+    {
+        $this->description = $this->group->description;
+        $this->user_id = $this->group->user_id;
+    }
+
     public function render()
     {
         return view('livewire.group.group-show');
