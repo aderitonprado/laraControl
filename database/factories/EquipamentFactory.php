0e40946b55d597dd49669ba83d75afb794ca81b3
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-23 11:17:38 -0300

    Update EquipamentFactory
    Inserindo informações na factory de equipamentos para criar equipamentos fakes para testes.

--- database/factories/EquipamentFactory.php
+++ database/factories/EquipamentFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class EquipamentFactory extends Factory
 {
@@ -14,7 +16,12 @@ class EquipamentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'description' => $this->faker->words(1, true),
+            'description_aux' => $this->faker->sentence,
+            'obs' => $this->faker->sentence,
+            'serial' => Str::random(10),
+            'status' => $this->faker->randomNumber(1),
+            'user_id' => 1,
         ];
     }
 }
