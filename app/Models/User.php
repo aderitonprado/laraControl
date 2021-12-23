4683ad3d206e7fcfe5ed2b64052af65ff49f27a4
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-23 11:15:45 -0300

    Update User.php
    Relacionamento do usaurio com o grupo, informamos que um usuario pode possuir vários grupos.

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Supply::class);
     }
+    
+    public function groups()
+    {
+        return $this->hasMany(Group::class);
+    }
 }
