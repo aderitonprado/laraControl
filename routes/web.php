e1a928ec8c91695f254e4d3e40f123b2569d1d88
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-23 13:57:49 -0300

    Update web.php
    Delete de componente não utilizado e criação das rotas de grupos e equipamentos.

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,18 @@
 use App\Http\Livewire\Supply\SupplyCreate;
 use App\Http\Livewire\Supply\SupplyEdit;
 use App\Http\Livewire\Supply\SupplyList;
-use App\Http\Livewire\Supply\SupplyPdf;
 use App\Http\Livewire\Supply\SupplyReports;
 use App\Http\Livewire\Supply\SupplyShow;
+
+/** Componente Groups */
+use App\Http\Livewire\Group\GroupCreate;
+use App\Http\Livewire\Group\GroupEdit;
+use App\Http\Livewire\Group\GroupList;
+use App\Http\Livewire\Group\GroupShow;
+
+/** Compomente Equipaments */
+
+
 use Illuminate\Support\Facades\Route;
 
 
@@ -27,6 +36,24 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
         Route::get('/edit/{supply}', SupplyEdit::class)->name('edit');
 
         Route::get('/reports', SupplyReports::class)->name('reports');
+
+    });
+
+    Route::prefix('groups')->name('groups.')->group(function(){
+        
+        Route::get('/', GroupList::class)->name('index');
+        Route::get('/show/{group}', GroupShow::class)->name('show');
+        Route::get('/create', GroupCreate::class)->name('create');
+        Route::get('/edit/{group}', GroupEdit::class)->name('edit');
+
+    });
+
+    Route::prefix('equipaments')->name('equipaments.')->group(function(){
+        
+        Route::get('/', SupplyList::class)->name('index');
+        Route::get('/show/{supply}', SupplyShow::class)->name('show');
+        Route::get('/create', SupplyCreate::class)->name('create');
+        Route::get('/edit/{supply}', SupplyEdit::class)->name('edit');
 
     });
 });
