ab051b93ab9b65fb8e2b13d3cb98703a35edad30
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-23 13:54:26 -0300

    Update GroupCreate.php
    Criação da classe createGroup, responsável por salvar os dados no banco.

--- app/Http/Livewire/Group/GroupCreate.php
+++ app/Http/Livewire/Group/GroupCreate.php
@@ -6,6 +6,21 @@ use Livewire\Component;
 
 class GroupCreate extends Component
 {
+    public $description;
+
+    protected $rules = ['description' => 'required|min:4'];
+
+    public function createGroup()
+    {
+        $this->validate();
+
+        auth()->user()->groups()->create([
+            'description' => $this->description
+        ]);
+
+        session()->flash('message', 'Grupo '.$this->description.' criado com sucesso!');
+    }
+
     public function render()
     {
         return view('livewire.group.group-create');
