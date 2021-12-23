f80796a9e8a7c46697bd8eb22e2b402ef7c1ecf0
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-23 11:20:01 -0300

    Update DatabaseSeeder.php
    Configurando nosso db seeder para gerar os dados de testes para os grupos e equipamentos.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\Group::factory(10)
+                ->hasEquipaments(10)
+                ->create();
     }
 }
