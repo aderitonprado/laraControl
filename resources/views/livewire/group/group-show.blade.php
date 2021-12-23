ecf4a0f5c6f322eae61106738ff73a3be683ed84
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-23 15:20:36 -0300

    Update group-show.blade.php
    Criação efetiva da view de visualizar grupo, retorna a descrição e o usuário de criação do grupo.

--- resources/views/livewire/group/group-show.blade.php
+++ resources/views/livewire/group/group-show.blade.php
@@ -1,3 +1,39 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <x-slot name="header">
+        <h1>Visualizar Grupo</h1>
+    </x-slot>
+
+    <div class="mt-4">
+
+        <div class="container">
+            <form name="showGroup">
+                <div class="row mb-3">
+                    <div class="col-sm-4">
+                        <div class="input-group">
+                            <input type="text" name="description"
+                                class="form-control"
+                                placeholder="Grupo" aria-label="description"
+                                aria-describedby="description" wire:model="description" disabled>
+                        </div>
+                    </div>
+                    
+                    <div class="col-sm-4">
+                        <div class="input-group">
+                            <input type="text" name="user_id"
+                                class="form-control"
+                                placeholder="Usuário" aria-label="user_id"
+                                aria-describedby="user_id" wire:model="user_id" disabled>
+                        </div>
+                    </div>
+
+                </div>
+
+                <a href="{{ route('groups.index') }}" class="btn btn-sm btn-info">Voltar</a>
+            </form>
+
+        </div>
+
+    </div>
+
+
 </div>
