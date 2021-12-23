157336f07a72218abaa9e643ccf67a8fd07815ed
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-23 13:55:04 -0300

    Update 2021_12_21_204459_create_groups_table.php
    Inserção da coluna user_id na migration de grupos.

--- database/migrations/2021_12_21_204459_create_groups_table.php
+++ database/migrations/2021_12_21_204459_create_groups_table.php
@@ -16,6 +16,7 @@ class CreateGroupsTable extends Migration
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->string('description');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
