40ab5bcd6bb23ce3ed457ebf47cd113d23e405b3
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-27 16:51:06 -0300

    Criação da lógica do componente Edit
    Criação de rules, mount e updateGroup.

--- app/Http/Livewire/Group/GroupEdit.php
+++ app/Http/Livewire/Group/GroupEdit.php
@@ -2,10 +2,38 @@
 
 namespace App\Http\Livewire\Group;
 
+use App\Models\Group;
 use Livewire\Component;
 
 class GroupEdit extends Component
 {
+    public $description;
+    public Group $group;
+
+    protected $rules = [
+        'description' => 'required|min:4',
+    ];
+
+    public function mount()
+    {
+        $this->description = $this->group->description;
+    }
+
+    public function updateGroup()
+    {
+        $this->validate();
+
+        $this->group->update([
+            'description' => $this->description,
+            //'userid_update'    => auth()->user()->id,
+
+        ]);
+
+        session()->flash('message', 'Grupo atualizado com sucesso!');
+
+        return redirect('/groups');
+    }
+
     public function render()
     {
         return view('livewire.group.group-edit');
