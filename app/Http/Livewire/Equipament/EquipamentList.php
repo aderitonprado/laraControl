81dc59c91ed21cb7c83a443b3a7cd0bded87c190
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-27 21:29:38 -0300

    Criação da lógica da tela de Listar Equipamentos
    Aninhamento do query builder para funcionar os filtros da tela.

--- app/Http/Livewire/Equipament/EquipamentList.php
+++ app/Http/Livewire/Equipament/EquipamentList.php
@@ -3,11 +3,39 @@
 namespace App\Http\Livewire\Equipament;
 
 use Livewire\Component;
+use App\Models\Equipament;
+use Livewire\WithPagination;
 
 class EquipamentList extends Component
 {
+    use WithPagination;
+
+    public $status = null;
+    public $start_date = null;
+    public $end_date = null;
+    public $take;
+
     public function render()
     {
-        return view('livewire.equipament.equipament-list');
+        $equipaments = Equipament::orderBy('id', 'DESC');
+
+        $equipaments->when($this->start_date, function ($queryBuilder) {
+            return $queryBuilder->where('created_at', '>=', $this->start_date);
+        });
+
+        $equipaments->when($this->end_date, function ($queryBuilder) {
+            return $queryBuilder->where('created_at', '<=', $this->end_date);
+        });
+
+        $equipaments->when($this->status, function ($queryBuilder) {
+            $this->status == "A" ? $this->status = 1 : $this->status = 0;
+            return $queryBuilder->where('status', '=', $this->status);
+        });
+
+        $equipaments = $this->take ? $equipaments->paginate($this->take) : $equipaments->get();
+
+        $equipaments = $equipaments->count() ? $equipaments : [];
+
+        return view('livewire.equipament.equipament-list', compact('equipaments'));
     }
 }
