8ceef936f8ef0d0027cb27b48a297329b0989826
Adériton Prado <aderitonpsantana@gmail.com>
2021-12-30 16:47:25 -0300

    Mudança na lógica do filtro de status
    Mudança na lógica do filtro de status, a lógica anterior estava causando um bug, aonde após selecionar o status do equipamento o select voltava sozinho para a posição 'Todos'.

--- app/Http/Livewire/Equipament/EquipamentList.php
+++ app/Http/Livewire/Equipament/EquipamentList.php
@@ -17,19 +17,37 @@ class EquipamentList extends Component
 
     public function render()
     {
+        // Retorna todos os equipamentos por ordem de ID Desc
         $equipaments = Equipament::orderBy('id', 'DESC');
 
+        /**
+         * Retorna os equipamentos respeitando a data de inicio do cadastro
+         */
         $equipaments->when($this->start_date, function ($queryBuilder) {
             return $queryBuilder->where('created_at', '>=', $this->start_date);
         });
 
+        /**
+         * Retorna os equipamentos respeitando a data final do cadastro
+         */
         $equipaments->when($this->end_date, function ($queryBuilder) {
             return $queryBuilder->where('created_at', '<=', $this->end_date);
         });
 
+        /**
+         * Retorna os equipamentos por status: Ativo / Inativo / Todos
+         */
         $equipaments->when($this->status, function ($queryBuilder) {
-            $this->status == "A" ? $this->status = 1 : $this->status = 0;
-            return $queryBuilder->where('status', '=', $this->status);
+
+            $sts = '';
+
+            if ($this->status != 'A' && $this->status != 'I') {
+                $sts = '';
+            }
+
+            $this->status == 'A' ? $sts = 1 : $sts = 0;
+
+            return $queryBuilder->where('status', '=', $sts);
         });
 
         $equipaments = $this->take ? $equipaments->paginate($this->take) : $equipaments->get();
