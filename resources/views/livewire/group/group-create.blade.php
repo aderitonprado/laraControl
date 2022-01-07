b85d8a02feb3b962c228b3c0e9b83d7c82c8597b
Adériton Prado <aderitonpsantana@gmail.com>
2022-01-07 10:56:11 -0300

    Remoção de classe btn-lg do botão criar grupos
    Remoção de classe btn-lg do botão criar grupos

--- resources/views/livewire/group/group-create.blade.php
+++ resources/views/livewire/group/group-create.blade.php
@@ -26,7 +26,7 @@
 
                 <button 
                     type="submit" 
-                    class="btn btn-lg btn-pill btn-success"
+                    class="btn btn-pill btn-success"
                     >
                     Criar Grupo
                 </button>
