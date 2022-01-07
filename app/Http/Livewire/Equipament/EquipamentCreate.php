7ab0b8e0b544ece08d672a1a328a4ebdf75a7dc6
Adériton Prado <aderitonpsantana@gmail.com>
2022-01-07 10:46:33 -0300

    Criação da lógica de criação de equipamentos
    Criação da lógica de criação de equipamentos no controller EquipamentCreate.

--- app/Http/Livewire/Equipament/EquipamentCreate.php
+++ app/Http/Livewire/Equipament/EquipamentCreate.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Livewire\Equipament;
 
+use App\Models\Group;
 use Livewire\Component;
 
 class EquipamentCreate extends Component
 {
+    public $equipament = [];
+
+    protected $rules = [
+        'equipament.description'       => 'required|string|min:4',
+        'equipament.description_aux'   => 'required',
+        'equipament.group_id'          => 'required|integer|min:1', 
+        'equipament.serial'            => 'required|min:1', 
+        'equipament.status'            => 'required|integer|min:1',
+    ];
+
+    public function createEquipament()
+    {
+        $this->validate();
+
+        auth()->user()->equipaments()->create($this->equipament);
+
+        session()->flash('message', 'Equipamento ' . $this->equipament['description'] . ' criado com sucesso!');
+
+        return redirect('/equipaments');
+    }
+
     public function render()
     {
-        return view('livewire.equipament.equipament-create');
+        $grupos = Group::all();
+
+        return view('livewire.equipament.equipament-create', compact('grupos'));
     }
 }
