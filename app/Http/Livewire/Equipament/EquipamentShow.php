4b66e8730ceb3d48782bf0ce885c3eed5f599307
Adériton Prado <aderitonpsantana@gmail.com>
2022-01-07 13:49:15 -0300

    Criação da lógica de mostrar equipamento do controller EquipamentShow
    Criação da lógica de mostrar equipamento do controller EquipamentShow

--- app/Http/Livewire/Equipament/EquipamentShow.php
+++ app/Http/Livewire/Equipament/EquipamentShow.php
@@ -2,10 +2,28 @@
 
 namespace App\Http\Livewire\Equipament;
 
+use App\Models\Equipament;
+use App\Models\Group;
 use Livewire\Component;
 
 class EquipamentShow extends Component
 {
+    public $equipaments = [];
+    public $grupos;
+    public Equipament $equipament;
+
+    public function mount()
+    {
+        $this->equipaments['description'] = $this->equipament->description;
+        $this->equipaments['description_aux'] = $this->equipament->description_aux;
+        $this->equipaments['group_id'] = $this->equipament->group_id;
+        $this->equipaments['obs'] = $this->equipament->obs;
+        $this->equipaments['serial'] = $this->equipament->serial;
+        $this->equipaments['status'] = $this->equipament->status;
+        $this->grupos = Group::all();
+ 
+    }
+
     public function render()
     {
         return view('livewire.equipament.equipament-show');
