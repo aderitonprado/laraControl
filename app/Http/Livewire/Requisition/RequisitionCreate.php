2781cfcca098ada87d99adcc072bcbd790bcb2b5
Adériton Prado <aderitonpsantana@gmail.com>
2022-01-14 17:27:21 -0300

    Criação do componente RequisitionCreate
    Criação da classe RequisitionCreate do componente RequisitionCreate.

---
+++ app/Http/Livewire/Requisition/RequisitionCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Requisition;
+
+use Livewire\Component;
+
+class RequisitionCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.requisition.requisition-create');
+    }
+}
