517dd27e5dbe7e2c0dd1d6b209040319e84eebbb
Adériton Prado <aderitonpsantana@gmail.com>
2022-01-14 17:28:12 -0300

    Criação do componente RequisitionEdit
    Criação da classe RequisitionEdit, do componente RequisitionEdit

---
+++ app/Http/Livewire/Requisition/RequisitionEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Requisition;
+
+use Livewire\Component;
+
+class RequisitionEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.requisition.requisition-edit');
+    }
+}
