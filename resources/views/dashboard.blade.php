38b8d7ac4b24b3e5502e10e560b4626e989a37ea
Adériton Prado <aderitonpsantana@gmail.com>
2024-03-25 12:40:45 -0300

    Update dashboard.blade.php
    Atualização do dashboard para aparecerem os primeiros gráficos de teste, usando o ChartJs.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,72 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <div class="container py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-               
+            <div class="overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="row bg-light">
+                    <div class="card col-sm-6">
+                        <div class="card-body">
+                            <div>
+                                <canvas id="myChart"></canvas>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card col-sm-6">
+                        <div class="card-body">
+                            <div>
+                                <canvas id="myChart2"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
+
+<script>
+    const ctx = document.getElementById('myChart');
+    const ctx2 = document.getElementById('myChart2');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+            datasets: [{
+                label: 'Abastecimento x Ano x 2023',
+                data: [12, 19, 3, 5, 2, 3, 7,8,9, 12,15,18],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    new Chart(ctx2, {
+        type: 'bar',
+        data: {
+            labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+            datasets: [{
+                label: 'Abastecimento x Ano x 2024',
+                data: [12, 19, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+</script>
